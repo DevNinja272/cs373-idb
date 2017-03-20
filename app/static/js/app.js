4d3475486921aaa4218ba79c78f991ab97f2e54b
Ben Nguyen <bpn252@cs.utexas.edu>
2017-03-20 14:24:29 -0500

    filled out degree table with static data

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -17,12 +17,25 @@ myApp.config(['$routeProvider',
                  templateUrl: '../static/partials/universities.html',
                  controller: 'UniversityCtrl'
              }).
+             when('/universities/:universityId', {
+                 templateUrl: '../static/partials/universitySpecific.html',
+                 controller: 'UniversitySpecificCtrl'
+             }).
              when('/states', {
                  templateUrl: '../static/partials/states.html',
                  controller: 'StateCtrl'
              }).
+             when('/states/:stateId', {
+                 templateUrl: '../static/partials/stateSpecific.html',
+                 controller: 'StateSpecificCtrl'
+             }).
              when('/degrees', {
                  templateUrl: '../static/partials/degrees.html',
+                 controller: 'DegreeCtrl'
+             }).
+             when('/degrees/:degreeId', {
+                 templateUrl: '../static/partials/degreeSpecific.html',
+                 controller: 'DegreeSpecificCtrl'
              }).
              otherwise({
                  redirectTo: '/'
--- app/static/js/controller.js
+++ app/static/js/controller.js
@@ -5,7 +5,11 @@ myApp.controller('UniversityCtrl',
         $scope.sortReverse  = false; 
         $scope.currentPage  = 1; 
 */
-        data = {"metadata":{"total":88,"page":2,"per_page":3},"results":[{"cost":22544,"school.name":"Stephen F Austin State University","school.ownership":1,"school.school_url":"www.sfasu.edu","size":10692},{"cost":37444,"school.name":"Texas Lutheran University","school.ownership":2,"school.school_url":"www.tlu.edu","size":1270},{"cost":18039,"school.name":"West Texas A & M University","school.ownership":1,"school.school_url":"www.wtamu.edu","size":7121}]}
+        data = 
+        {"metadata":{"total":88,"page":2,"per_page":3},"results":[
+        {"id":1, "cost":22544,"school.name":"Stephen F Austin State University","school.ownership":1,"school.school_url":"www.sfasu.edu","size":10692},
+        {"id":2, "cost":37444,"school.name":"Texas Lutheran University","school.ownership":2,"school.school_url":"www.tlu.edu","size":1270},
+        {"id":3, "cost":18039,"school.name":"West Texas A & M University","school.ownership":1,"school.school_url":"www.wtamu.edu","size":7121}]}
         for(var i = 0; i < data.results.length; i++) {
             if (data.results[i]["school.ownership"] == 1){
                 data.results[i]["school.ownership"] = "Public";
@@ -17,6 +21,13 @@ myApp.controller('UniversityCtrl',
         $scope.results = data["results"]
     }]);
 
+myApp.controller('UniversitySpecificCtrl',
+    ['$scope',
+    function($scope) {
+        $scope.hello = 'hello from university specific!'
+        
+    }]);
+
 myApp.controller('StateCtrl',
     ['$scope',
     function($scope) {
@@ -24,6 +35,39 @@ myApp.controller('StateCtrl',
         $scope.sortReverse  = false; 
         $scope.currentPage  = 1; 
 */
-        data = {"results":[{"average_public_cost":35987,"name": "Texas","average_private_cost":67234,"schools":98,"region":"SouthWest"},{"average_public_cost":40726,"name": "Florida","average_private_cost":57873,"schools":54,"region":"SouthEast"},{"average_public_cost":35733,"name": "Utah","average_private_cost":61283,"schools":10,"region":"Rocky Mountains"}]}
+        data = 
+        {"results":[
+        {"id":1, "average_public_cost":35987,"name": "Texas","average_private_cost":67234,"schools":98,"region":"SouthWest"},
+        {"id":2, "average_public_cost":40726,"name": "Florida","average_private_cost":57873,"schools":54,"region":"SouthEast"},
+        {"id":3, "average_public_cost":35733,"name": "Utah","average_private_cost":61283,"schools":10,"region":"Rocky Mountains"}]}
         $scope.results = data["results"]
+    }]);
+
+myApp.controller('StateSpecificCtrl',
+    ['$scope',
+    function($scope) {
+        $scope.hello = 'hello from state specific!'
+        
+    }]);
+
+myApp.controller('DegreeCtrl',
+    ['$scope',
+    function($scope) {
+        /*$scope.sortType     = 'title';
+        $scope.sortReverse  = false; 
+        $scope.currentPage  = 1; 
+*/
+        data = 
+        {"results":[
+        {"id":1, "name": "Education","numPub":3423,"numPriv":1231,"AwardedPub":"10%","AwardedPriv":"25%" },
+        {"id":2, "name": "Engineering","numPub":4213,"numPriv":2349,"AwardedPub":"17%","AwardedPriv":"12%"},
+        {"id":3, "name": "Psychology","numPub":1023,"numPriv":976,"AwardedPub":"11%","AwardedPriv":"5%"}]}
+        $scope.results = data["results"]
+    }]);
+
+myApp.controller('DegreeSpecificCtrl',
+    ['$scope',
+    function($scope) {
+        $scope.hello = 'hello from degree specific'
+        
     }]);
