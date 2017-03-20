9e9fb0be859bb22aaec44800c041de207798caee
Ahmed Al Nabil <nabilthenabil@gmail.com>
2017-03-20 13:10:13 -0500

    Added possible test cases to be filled out later

--- app/test.py
+++ app/test.py
@@ -1,9 +1,31 @@
 """
 from unittest import main, TestCase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import University, State, Degrees
+from config import test_db
 
 class TestModels (TestCase):
 
-    def test_1(self):
+    def test(self):
+
+    def test_university_1(self):
+
+    def test_university_2(self):
+
+    def test_university_3(self):
+
+    def test_state_1(self):
+
+    def test_state_2(self):
+
+    def test_state_3(self):
+
+    def test_degrees_1(self):
+
+    def test_degrees_2(self):
+
+    def test_degrees_3(self):
 
 if __name__ == "__main__":
     main()
