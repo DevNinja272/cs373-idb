56fab579052e8d299a67bab533a896284ead0acd
Faisal Aldilaijan <faisald@utexas.edu>
2017-03-22 12:43:39 -0500

    updated comments location in model.py

--- app/model.py
+++ app/model.py
@@ -1,20 +1,25 @@
+"""
+Model.py is used to represent all the tables in sqlalchemy database.
+"""
+
 from sqlalchemy import Table, Column, Float, Integer, String, Boolean, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-"""
-Created 5 column attributes of University model: 
-1. Name of the university 
-2. Number of students
-3. Whether school is public or not
-4. URL of school's website
-5. Academic cost
-Linked the University model to the DegreesUniversities model to check all the degrees offered by the university
-Linked the University model to the State model to check what state a university is from
-"""
+
 class University(Base):
+    """
+    Created 5 column attributes of University model: 
+    1. Name of the university 
+    2. Number of students
+    3. Whether school is public or not
+    4. URL of school's website
+    5. Academic cost
+    Linked the University model to the DegreesUniversities model to check all the degrees offered by the university
+    Linked the University model to the State model to check what state a university is from
+    """
     __tablename__ = 'university'
     id = Column(Integer, primary_key=True)
 
@@ -31,16 +36,17 @@ class University(Base):
         return "<University(name={}, num_students={}, is_public={}, website_URL={}, academic_cost={}"\
             .format(self.name, self.num_students, self.is_public, self.website_URL, self.academic_cost)
 
-"""
-Created 5 column attributes of State model: 
-1. Name of state 
-2. Region state is located
-3. Average public tuition in the state
-4. Average private tuition in the state
-5. Number of colleges in the state
-Linked the State model to to the University model to find all the universities in a given state
-"""
+
 class State(Base):
+    """
+    Created 5 column attributes of State model: 
+    1. Name of state 
+    2. Region state is located
+    3. Average public tuition in the state
+    4. Average private tuition in the state
+    5. Number of colleges in the state
+    Linked the State model to to the University model to find all the universities in a given state
+    """
     __tablename__ = 'state'
     id = Column(Integer, primary_key=True)
 
@@ -55,16 +61,17 @@ class State(Base):
         return "<State(name={}, region={}, average_public_tuition={}, average_private_tuition={}, number_colleges={}"\
             .format(self.name, self.region, self.average_public_tuition, self.average_private_tuition, self.number_colleges)
 
-"""
-Created 5 column attributes of Degree model: 
-1. Name of degree 
-2. Number of public schools that offer degree
-3. Number of private schools that offer degree
-4. Percent of public schools that offer degree
-5. Percent of private schools that offer degree
-Linked Degree model to DegreeUniversities model to check all the universities that offer the degree
-"""
+
 class Degree(Base):
+    """
+    Created 5 column attributes of Degree model: 
+    1. Name of degree 
+    2. Number of public schools that offer degree
+    3. Number of private schools that offer degree
+    4. Percent of public schools that offer degree
+    5. Percent of private schools that offer degree
+    Linked Degree model to DegreeUniversities model to check all the universities that offer the degree
+    """
     __tablename__ = 'degree'
     id = Column(Integer, primary_key=True)
 
@@ -80,10 +87,13 @@ class Degree(Base):
             , num_percent_public= {}, num_percent_private={}".format(self.name, self.num_public_offer,\
                 self.num_private_offer, self.num_percent_public, self.num_percent_private)
 
-# Reference many to many: http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html#many-to-many
-"Helps link the Degrees model and University model together to help check all the degrees \
- offered by a university and all the universities that offer a degree"
+
 class DegreesUniversities(Base):
+    """
+    Helps link the Degrees model and University model together to help check all the degrees 
+    offered by a university and all the universities that offer a degree. Got help from this reference many to many:
+    http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html#many-to-many
+    """
     __tablename__ = 'degreesUniversities'
 
     id = Column(Integer, primary_key=True)
