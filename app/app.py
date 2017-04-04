cce684d4d2d9e35c04f6274b47b9d2ae1ecb015f
aldilaff <aldilaff@gmail.com>
2017-04-04 17:48:09 -0500

    upated travis.yml to install reqs. Changed response for collective end points

--- app/app.py
+++ app/app.py
@@ -76,18 +76,7 @@ def get_unis():
   session = Session()
   universities = []
   for uni in session.query(University).all():
-    state = session.query(State).filter(State.id == uni.state_id).one()
-    uni.state_name = str(state.name)
-    uni.state_id = state.id
-
-    degrees = session.query(DegreesUniversities).filter(DegreesUniversities.university_id == uni.id).all()
-    list_degree_ids = []
-    for degree in degrees:
-      list_degree_ids.append(degree.degree_id)
-    print(list_degree_ids)
-
     uni_dict = uni.__dict__.copy()
-    uni_dict['degrees'] = list_degree_ids
     uni_dict.pop('_sa_instance_state', None)
     universities.append(uni_dict)
   return jsonify(universities=universities)
@@ -96,15 +85,7 @@ def get_unis():
 def get_states():
   session = Session()
   states = []
-  for state in session.query(State).all():
-
-    state.unis = []
-    for uni in session.query(University).all():
-      if str(uni.state_id) == str(state.id):
-        uni_dict = uni.__dict__.copy()
-        uni_dict.pop('_sa_instance_state', None)
-        state.unis.append(uni_dict)
-
+  for state in session.query(State).all():  
     state_dict = state.__dict__.copy()
     state_dict.pop('_sa_instance_state', None)
     states.append(state_dict)
@@ -116,13 +97,7 @@ def get_degrees():
   session = Session()
   degrees = []
   for degree in session.query(Degree).all():
-    universities = session.query(DegreesUniversities).filter(DegreesUniversities.degree_id == degree.id).all()
-    list_university_ids = []
-    for university in universities:
-      list_university_ids.append(university.university_id)
-
     degree_dict = degree.__dict__.copy()
-    degree_dict['universities'] = list_university_ids
     degree_dict.pop('_sa_instance_state', None)
     degrees.append(degree_dict)
 
